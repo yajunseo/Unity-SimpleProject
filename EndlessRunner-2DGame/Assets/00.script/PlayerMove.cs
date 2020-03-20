e303f5b591536e87886e339f62ac84aaaa4c89e7
yajunseo  - C105 <55369207+yajunseo@users.noreply.github.com>
2020-03-21 03:37:37 +0900

    player move, gound set, mob set
    플레이어 점프, 바닥 무한 순환 구현, 몹 움직임 구현 완료

--- EndlessRunner-2DGame/Assets/00.script/PlayerMove.cs
+++ EndlessRunner-2DGame/Assets/00.script/PlayerMove.cs
@@ -2,17 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMove : MonoBehaviour
 {
+    bool isJump = false;
+    bool isTop = false;
+    public float jumpHeight = 0;
+    public float jumpSpeed = 0;
+
+    Vector2 startPosition;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = transform.position;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        animator.SetBool("isRun", true);
+        if (Input.GetMouseButtonDown(0))
+        {
+            isJump = true;
+        }
+        else if (transform.position.y <= startPosition.y)
+        {
+            isJump = false;
+            isTop = false;
+            transform.position = startPosition;
+        }
+
+        if (transform.position.y <= jumpHeight - 0.1f && !isTop)
+        {
+            transform.position = Vector2.Lerp(transform.position, new Vector2(transform.position.x, jumpHeight), jumpSpeed * Time.deltaTime);
+        }
+        else
+        {
+            isTop = true;
+        }
+        if (transform.position.y > startPosition.y && isTop)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, startPosition, jumpSpeed * Time.deltaTime);
+        }
     }
 }
