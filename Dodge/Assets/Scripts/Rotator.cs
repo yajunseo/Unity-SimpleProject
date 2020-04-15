015a6007d130f467c1c422741cf736bb25c21b19
yajunseo  - C105 <55369207+yajunseo@users.noreply.github.com>
2020-04-15 19:34:26 +0900

    Time UI, Map rotation
    시간 점수 ui 구현 진행 중,
    맵 회전 구현 완료.

---
+++ Dodge/Assets/Scripts/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotationSpeed = 60f;
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+    }
+}
