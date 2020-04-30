da87a8b1076e374a48eb96eee05c2e02e181da9b
yajunseo  - C105 <55369207+yajunseo@users.noreply.github.com>
2020-05-01 02:08:33 +0900

    brain virtual camera setting
    카메라 세팅(바디, 에임) 완료

--- ZombiSurvival/Zombie/Assets/Scripts/PlayerInput.cs
+++ ZombiSurvival/Zombie/Assets/Scripts/PlayerInput.cs
@@ -25,7 +25,7 @@ public class PlayerInput : MonoBehaviour {
             reload = false;
             return;
         }
-
+       
         // move에 관한 입력 감지
         move = Input.GetAxis(moveAxisName);
         // rotate에 관한 입력 감지
