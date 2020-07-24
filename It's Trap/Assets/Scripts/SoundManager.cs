9966211da9202782bbfa28a83ec4b0e2653fd7fe
yajunseo  - C105 <55369207+yajunseo@users.noreply.github.com>
2020-07-25 01:07:47 +0900

    bgm 설정 완료
    랜덤 bgm 2개 설정 완료

---
+++ It's Trap/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound 
+{
+    public string soundName;
+    public AudioClip clip;
+}
+
+public class SoundManager : MonoBehaviour
+{
+    [Header("사운드 등록")]
+    [SerializeField] Sound[] bgmSounds;
+
+    [Header("브금 플레이어")]
+    [SerializeField] AudioSource bgmPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayRandomBGM();
+    }
+
+    public void PlayRandomBGM()
+    {
+        int random = Random.Range(0, 2);
+        bgmPlayer.clip = bgmSounds[random].clip;
+        bgmPlayer.Play();
+    }
+}
