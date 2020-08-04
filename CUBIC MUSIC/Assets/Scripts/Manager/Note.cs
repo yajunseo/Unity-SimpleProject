2af3b13abd730051ba03373dbde395cb06bbf459
yajunseo  - C105 <55369207+yajunseo@users.noreply.github.com>
2020-08-04 23:49:27 +0900

    Cubic Music 프로젝트 시작
    새로운 프로젝트 리듬 게임 시작

---
+++ CUBIC MUSIC/Assets/Scripts/Manager/Note.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    public float noteSpeed = 400;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition += Vector3.right * noteSpeed * Time.deltaTime;  // local 을 안하면 캔버스 좌표가 아닌 월드 좌표로 이동하니 주의!!!
+    }
+}
---
+++ CUBIC MUSIC/Assets/Scripts/Manager/NoteManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteManager : MonoBehaviour
+{
+    public int bpm = 0;
+    double currentTime = 0d;
+
+    [SerializeField] Transform tfNoteAppear = null;
+    [SerializeField] GameObject goNote = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
