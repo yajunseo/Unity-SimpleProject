38d465b6401a4ceea8b83553da941b709fb4b0b6
yajunseo  - C105 <55369207+yajunseo@users.noreply.github.com>
2020-08-05 01:50:36 +0900

    노트 설정 및 충돌구현 완료
    노트 설정 및 충돌, 노래 시작 구현 완료

--- CUBIC MUSIC/Assets/Scripts/Manager/NoteManager.cs
+++ CUBIC MUSIC/Assets/Scripts/Manager/NoteManager.cs
@@ -10,15 +10,25 @@ public class NoteManager : MonoBehaviour
     [SerializeField] Transform tfNoteAppear = null;
     [SerializeField] GameObject goNote = null;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentTime += Time.deltaTime;
+
+        if(currentTime >= 60d / bpm)
+        {
+            GameObject t_note = Instantiate(goNote, tfNoteAppear.position, Quaternion.identity);
+            t_note.transform.SetParent(this.transform); // 이스크립트가 붙어있는 객체를 부모로 설정
+            currentTime -= 60d / bpm;    // 0 안됨 currenttime이 딱 떨어지는게 아닌 0.51005551 이런식으로 되므로 0.01005의 오차가 손실됨
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Note"))
+        {
+            Destroy(collision.gameObject);
+        }
     }
 }
