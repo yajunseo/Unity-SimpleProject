de023956b2d952b99206b76765f43b88aad97187
yajunseo  - C105 <55369207+yajunseo@users.noreply.github.com>
2020-09-03 02:51:12 +0900

    오브젝트풀링 연습
    유니티 스킬 공부
    오브젝트풀링 연습

---
+++ UnitySkill/Assets/createCube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createCube : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CreateCoroutine());
+    }
+
+    IEnumerator CreateCoroutine()
+    {
+        while(true)
+        {
+            yield return null;
+            GameObject t_object = ObjectPoolingManager.instance.GetQueue();
+            t_object.transform.position = Vector3.zero;
+        }
+    }
+}
---
+++ UnitySkill/Assets/cube.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour
+{
+    Rigidbody m_myrigid = null;
+
+    private void OnEnable()
+    {
+        if(m_myrigid == null)
+        {
+            m_myrigid = GetComponent<Rigidbody>();
+        }
+
+        m_myrigid.velocity = Vector3.zero;
+        m_myrigid.AddExplosionForce(1000, transform.position, 1f);
+
+        StartCoroutine(DestroyCube());
+    }
+
+    IEnumerator DestroyCube()
+    {
+        yield return new WaitForSeconds(1f);
+        ObjectPoolingManager.instance.InsertQueue(gameObject);
+    }
+}
