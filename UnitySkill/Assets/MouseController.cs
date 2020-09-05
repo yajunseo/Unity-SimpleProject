e7eae5b16cbbe16bf792d77b2f5da8ac49c23fa9
yajunseo  - C105 <55369207+yajunseo@users.noreply.github.com>
2020-09-06 02:18:44 +0900

    폭발 파편 효과
    유니티 스킬 연습
    폭발 파편 효과

---
+++ UnitySkill/Assets/MouseController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+  
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray t_ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit t_hit;
+            if(Physics.Raycast(t_ray, out t_hit, 100f))
+            {
+                t_hit.transform.GetComponent<CubeDebris>().Explosion();
+            }
+        }
+    }
+}
