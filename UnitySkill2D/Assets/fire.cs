caf5c897532c365f039ce02db7864f45b7e407b8
Ya Jun Seo <yajunseo313@naver.com>
2020-09-09 22:09:25 +0900

    포물선 발사
    유니티 스킬 연습
    포물선 발사 및
    오브젝트 풀링 활용

---
+++ UnitySkill2D/Assets/fire.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fire : MonoBehaviour
+{
+    [SerializeField] Transform m_tfArrow = null;
+
+    Camera m_cam = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_cam = Camera.main;
+    }
+    void LookatMouse()
+    {
+        Vector2 t_mousePos = m_cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 t_direction = new Vector2(t_mousePos.x - m_tfArrow.position.x,
+                                            t_mousePos.y - m_tfArrow.position.y);
+
+        m_tfArrow.right = t_direction;
+    }    
+
+    void TryFire()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject temp = objectpoolingManager.instance.GetQueue();
+            temp.transform.position = m_tfArrow.position;
+            temp.transform.rotation = m_tfArrow.rotation;
+            temp.transform.right = m_tfArrow.right;
+            temp.GetComponent<Rigidbody2D>().velocity = m_tfArrow.transform.right * 10f;
+
+            StartCoroutine(DestroyArrow(temp));
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookatMouse();
+        TryFire();
+    }
+
+    IEnumerator DestroyArrow(GameObject gameobject)
+    {
+        yield return new WaitForSeconds(2f);
+        objectpoolingManager.instance.InsertQueue(gameobject);
+    }
+}
---
+++ UnitySkill2D/Assets/objectpoolingManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectpoolingManager : MonoBehaviour
+{
+    public static objectpoolingManager instance;
+    public Queue<GameObject> m_queue = new Queue<GameObject>();
+    [SerializeField] GameObject goProfab = null;
+    [SerializeField] Transform m_tfArrow = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+
+        for(int i=0;i<50;i++)
+        {
+            GameObject temp = Instantiate(goProfab, m_tfArrow.position, Quaternion.identity);
+            temp.SetActive(false);
+            m_queue.Enqueue(temp);
+        }
+    }
+
+    public void InsertQueue(GameObject p_object)
+    {
+        p_object.SetActive(false);
+        m_queue.Enqueue(p_object);
+    }
+
+    public GameObject GetQueue()
+    {
+        GameObject temp = m_queue.Dequeue();
+        temp.SetActive(true);
+        return temp;
+    }
+}
